ca77e94627cf704947508f49f82ab7a9ec1d5f35
Dango <jason543211@hotmail.com>
2022-09-14 04:11:46 -0700

    chore: searchBar interaction

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,30 +1,47 @@
 import { useState } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { XCircleIcon } from '@heroicons/react/20/solid'
-import { Input } from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
 
 const SearchBar: React.FC = () => {
   const [searchText, setSearchText] = useState('')
+  const [isInputFocused, setIsInputFocused] = useState(false)
 
   return (
     <>
       <div
-        className="h-[44px] w-[280px] bg-bgGrey rounded-[12px] flex items-center justify-start 
-       gap-2 cursor-text"
+        className="h-[44px] w-[300px] bg-bgGrey rounded-[12px] flex items-center justify-between  
+       cursor-text"
         placeholder="Search"
       >
-        <MagnifyingGlassIcon className="h-[24px] w-[24px] text-textGrey ml-[16px]" />
+        <MagnifyingGlassIcon className="h-[22px] w-[22px] text-textGrey ml-[16px]" />
         <input
-          className="h-[40px] w-[200px] bg-transparent border-none"
-          placeholder="Search"
+          className="h-[40px] w-[160px] bg-transparent border-none"
+          placeholder="Search..."
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
         />
-        {searchText && (
+
+        {searchText ? (
+          <div
+            className="bg-transparent border-none cursor-pointer hover:bg-slate-300/10 rounded-[20px] p-2 text-slate-300"
+            onClick={() => setSearchText('')}
+          >
+            Clear
+          </div>
+        ) : (
+          <div className="w-[50px]" />
+        )}
+
+        {isInputFocused ? (
           <XCircleIcon
-            className="h-[20px] w-[20px] text-textGrey mr-[16px] text-slate-400 cursor-pointer"
+            className="h-[20px] w-[20px] text-textGrey text-slate-400 cursor-pointer mr-[10px]"
             onClick={() => setSearchText('')}
           />
+        ) : (
+          <div className=" h-[20px] w-[20px] mr-[10px]" />
         )}
       </div>
     </>

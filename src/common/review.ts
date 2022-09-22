ac79f50513ccd5a64d338c6aea076619c2e289bb
Dango <jason543211@hotmail.com>
2022-09-21 23:17:57 -0700

    chore: review post emoji

--- src/common/review.ts
+++ src/common/review.ts
@@ -5,20 +5,23 @@ import type { ParseReivewPostInput } from '@/types/review'
 // organize and format review post
 export const parseReviewPost = (input: ParseReivewPostInput): string => {
   const mediaType = input.mediaType === 'movie' ? 'Movie 🎬' : 'TV 📺'
-  // description with a max length of 200 characters
   const description =
     input.mediaDescription.length > 200 ? `${input.mediaDescription.substring(0, 200)}...` : input.mediaDescription
+  const stars = '🟣'.repeat(input.reviewRating)
+  const emptyStars = '⚪'.repeat(10 - input.reviewRating)
+  const reviewHeadlineEmoji = '📌'
+  const reviewContentEmoji = '📝'
 
   const post = `
 ${mediaType} Review - ${input.mediaName} (${input.mediaYear.slice(0, 4)}) 
 
 ${description}
 
-Rating: ${input.reviewRating}/10
+Rating: ${stars}${emptyStars} (${input.reviewRating}/10)
 
-${input.reviewHeadline}
+${reviewHeadlineEmoji} ${input.reviewHeadline}
 
-${input.reviewContent}
+${reviewContentEmoji} ${input.reviewContent}
   `
 
   return post

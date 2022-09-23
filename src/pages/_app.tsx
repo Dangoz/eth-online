51d48640d95c91fbcc1dd3fb939cc9ea28a18000
Dango <jason543211@hotmail.com>
2022-09-23 14:13:15 -0700

    fix: NavBar (NavBar above root component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,8 +24,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <WagmiConfig client={wagmiClient}>
             <RainbowKitProvider chains={chains} theme={darkTheme()}>
               <NextUIProvider theme={theme}>
-                <Component {...pageProps} />
                 <Navbar />
+                <Component {...pageProps} />
                 <ToastContainer />
                 <RelayChat />
               </NextUIProvider>

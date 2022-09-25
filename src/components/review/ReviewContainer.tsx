cfc1b659f53e27705efd0cac664c796f15ea854c
Dango <jason543211@hotmail.com>
2022-09-24 17:24:59 -0700

    chore: reviewContainer

---
+++ src/components/review/ReviewContainer.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react'
+
+const ReviewContainer: React.FC = () => {
+  return <div>ReviewContainer</div>
+}
+
+export default ReviewContainer
---
+++ src/pages/review/[id].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import ReviewContainer from '@/components/review/ReviewContainer'
+
+const Review: NextPage = () => {
+  return (
+    <>
+      <ReviewContainer />
+    </>
+  )
+}
+
+export default Review

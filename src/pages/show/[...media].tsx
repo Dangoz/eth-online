bef632f53381210df76d523d12a7192c50928e8a
Dango <jason543211@hotmail.com>
2022-09-25 04:25:41 -0700

    fix: debug

--- src/pages/show/[...media].tsx
+++ src/pages/show/[...media].tsx
@@ -6,39 +6,40 @@ import tmdb from '@/common/tmdb'
 const Show: NextPage<{ media: Media }> = ({ media }) => {
   return (
     <>
-      <ShowContainer media={media} />
+      {/* <ShowContainer media={media} /> */}
+      123
     </>
   )
 }
 
 export default Show
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    const pathArray = context?.params?.media as string[]
-    if (pathArray[0] !== 'movie' && pathArray[0] !== 'tv') {
-      return {
-        notFound: true,
-      }
-    }
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   try {
+//     const pathArray = context?.params?.media as string[]
+//     if (pathArray[0] !== 'movie' && pathArray[0] !== 'tv') {
+//       return {
+//         notFound: true,
+//       }
+//     }
 
-    const { media, error } = await tmdb.getDetails(+pathArray[1], pathArray[0])
-    if (error !== null) {
-      return {
-        notFound: true,
-      }
-    }
+//     const { media, error } = await tmdb.getDetails(+pathArray[1], pathArray[0])
+//     if (error !== null) {
+//       return {
+//         notFound: true,
+//       }
+//     }
 
-    return {
-      props: {
-        media,
-      },
-    }
-  } catch (err) {
-    const error = err as any
-    console.error(error?.data?.message || error?.message || error)
-    return {
-      notFound: true,
-    }
-  }
-}
+//     return {
+//       props: {
+//         media,
+//       },
+//     }
+//   } catch (err) {
+//     const error = err as any
+//     console.error(error?.data?.message || error?.message || error)
+//     return {
+//       notFound: true,
+//     }
+//   }
+// }
